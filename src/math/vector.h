8e627b8f0ac0f64fed5b44a9f07ee97aed314912
wankun.huang <pisnebula@gmail.com>
2026-01-22 22:58:13 +0800

    [特性新增] #time 1h 新增向量别名

--- src/math/vector.h
+++ src/math/vector.h
@@ -16,6 +16,20 @@ template <class T>
 class Vector3;
 template <class T>
 class Vector4;
+template <class T>
+class Vector2;
+
+using Vector2i = Vector2<int>;
+using Vector2d = Vector2<double>;
+using Vector2f = Vector2<float>;
+
+using Vector3i = Vector3<int>;
+using Vector3d = Vector3<double>;
+using Vector3f = Vector3<float>;
+
+using Vector4i = Vector4<int>;
+using Vector4d = Vector4<double>;
+using Vector4f = Vector4<float>;
 
 /**
  * @brief 二维向量类模板
